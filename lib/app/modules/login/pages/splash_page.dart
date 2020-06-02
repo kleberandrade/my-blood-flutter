c543279eda7599640c2849d10ee8476279ba1b41
Jessica Pereira <jessica.pereira@sage.com>
2020-06-01 22:41:14 -0300

    teste auto login

--- lib/app/modules/login/pages/splash_page.dart
+++ lib/app/modules/login/pages/splash_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_blood/app/modules/home/pages/home_page.dart';
 import 'package:my_blood/app/modules/login/pages/sign_in_page.dart';
 import 'package:splashscreen/splashscreen.dart';
 
@@ -10,10 +12,31 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.currentUser().then((res) {
+      print(res);
+      if (res != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
+      else
+      {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignInPage()),
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SplashScreen(
       seconds: 8,
-      navigateAfterSeconds: new SignInPage(),
+      //navigateAfterSeconds: user != null ? new HomePage() : new SignInPage(),
       backgroundColor: Theme.of(context).primaryColor,
       loaderColor: Theme.of(context).canvasColor,
       photoSize: 75.0,
